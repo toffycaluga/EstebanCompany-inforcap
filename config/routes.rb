7d9fc82e7f775a06e165dddd9c64874aad274cab
Toffy Caluga <abraham.lillol@gmail.com>
2023-07-28 13:23:38 -0300

    FEAT:Generando rotas root segun usuario

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  get 'home/index'
   resources :job_offers
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "job_offers#index"
+  authenticated :user do
+    root to: 'job_offers#index', as: :authenticated_root
+  end
+
+  unauthenticated do
+    root to: 'home#index', as: :unauthenticated_root
+  end
 end
